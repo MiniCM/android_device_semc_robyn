4c80c857be854db9bd0754c3114686518dab49c0
Eran <eranmizra@gmail.com>
2010-10-25 20:30:21 +0200

    flykernel-11b

--- device_hero.mk
+++ device_hero.mk
@@ -23,7 +23,7 @@ endif
 PRODUCT_COPY_FILES += \
     $(LOCAL_KERNEL):kernel
 
-KERNEL_NAME := 2.6.29.6-flykernel-11
+KERNEL_NAME := 2.6.29.6-flykernel-11b
 
 PRODUCT_COPY_FILES += \
     device/htc/hero/modules/modules.dep.bin:system/lib/modules/$(KERNEL_NAME)/modules.dep.bin \
