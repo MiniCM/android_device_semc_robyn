8b9a7bc09040af5e8a936073649d6e387973ba02
Vassilis Tsogkas <tsogkas@ceid.upatras.gr>
2011-04-09 14:29:51 +0300

    Fixes for rmmod and OV module.
    Change-Id: I503f7919093ba020eb7659a64d8bb9bad7c78245

--- extract-files.sh
+++ extract-files.sh
@@ -325,7 +325,7 @@ PRODUCT_COPY_FILES += \\
     device/htc/__DEVICE__/prebuilt/twofish.ko:system/lib/modules/twofish.ko \\
     device/htc/__DEVICE__/prebuilt/twofish_common.ko:system/lib/modules/twofish_common.ko \\
     device/htc/__DEVICE__/prebuilt/x8gesture.ko:system/lib/modules/x8gesture.ko \\
-    device/htc/__DEVICE__/prebuilt/x8oc.ko:system/lib/modules/x8gesture.ko
+    device/htc/__DEVICE__/prebuilt/x8oc.ko:system/lib/modules/x8oc.ko
 
 PRODUCT_COPY_FILES += \\
     vendor/htc/__DEVICE__/proprietary/kexec-tool:system/bin/kexec-tool \\
